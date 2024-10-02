9de80d06365b90aa6e114dd6c7fc2be1c0879f00
NΞVΛR <danielo-24@hotmail.com>
2024-10-02 08:46:26 +0200

    Ajoutez des fichiers projet.

---
+++ ClientCC/Client.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreditCeleste
+{
+
+        public class Client
+        {
+            private string civClient;
+            private string nomClient;
+            private string prenomClient;
+
+            public Client()
+            {
+                civClient = "Mr";
+                nomClient = "Soares";
+                prenomClient = "Daniels";
+
+            }
+
+            public Client(string CivCli)
+            {
+                civClient = CivCli;
+                nomClient = "Soares";
+                prenomClient = "Daniels";
+            }
+
+            public Client(string CivCli, string nomCli)
+            {
+                civClient = CivCli;
+                nomClient = nomCli;
+                prenomClient = "Daniels";
+            }
+
+
+            public Client(string CivCli, string nomCli, string prenomCli)
+            {
+                civClient = CivCli;
+                nomClient = nomCli;
+                prenomClient = prenomCli;
+            }
+
+
+            public void setClient(string CivCli, string nomCli, string prenomCli)
+            {
+                civClient = CivCli;
+                nomClient = nomCli;
+                prenomClient = prenomCli;
+            }
+
+            public string getCivClient() { return civClient; }
+            public string getNomClient() { return nomClient; }
+            public string getPrenomClient() { return prenomClient; }
+
+
+        }
+ }
