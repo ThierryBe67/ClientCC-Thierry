d64bc066862ff20394b7fe3cd46145364aeabebf
OrangeZXZ <morangezxz@gmail.com>
2024-12-18 10:39:59 +0100

    Test + Nouvelle Indentation

--- ClientCC/Client.cs
+++ ClientCC/Client.cs
@@ -6,55 +6,50 @@ using System.Threading.Tasks;
 
 namespace CreditCeleste
 {
+    public class Client
+    {
+        private string civClient;
+        private string nomClient;
+        private string prenomClient;
 
-        public class Client
+        public Client()
         {
-            private string civClient;
-            private string nomClient;
-            private string prenomClient;
-
-            public Client()
-            {
-                civClient = "Mr";
-                nomClient = "TirLudAleDan";
-                prenomClient = "TLAD";
-
-            }
-
-            public Client(string CivCli)
-            {
-                civClient = CivCli;
-                nomClient = "TirLudAleDan";
-                prenomClient = "TLAD";
-            }
-
-            public Client(string CivCli, string nomCli)
-            {
-                civClient = CivCli;
-                nomClient = nomCli;
-                prenomClient = "TLAD";
-            }
-
-
-            public Client(string CivCli, string nomCli, string prenomCli)
-            {
-                civClient = CivCli;
-                nomClient = nomCli;
-                prenomClient = prenomCli;
-            }
-
-
-            public void setClient(string CivCli, string nomCli, string prenomCli)
-            {
-                civClient = CivCli;
-                nomClient = nomCli;
-                prenomClient = prenomCli;
-            }
-
-            public string getCivClient() { return civClient; }
-            public string getNomClient() { return nomClient; }
-            public string getPrenomClient() { return prenomClient; }
-
+            civClient = "Mr";
+            nomClient = "TirLudAleDan";
+            prenomClient = "TLAD";
 
         }
- }
+
+        public Client(string CivCli)
+        {
+            civClient = CivCli;
+            nomClient = "TirLudAleDan";
+            prenomClient = "TLAD";
+        }
+
+        public Client(string CivCli, string nomCli)
+        {
+            civClient = CivCli;
+            nomClient = nomCli;
+            prenomClient = "TLAD";
+        }
+
+        public Client(string CivCli, string nomCli, string prenomCli)
+        {
+            civClient = CivCli;
+            nomClient = nomCli;
+            prenomClient = prenomCli;
+        }
+
+        public void setClient(string CivCli, string nomCli, string prenomCli)
+        {
+            civClient = CivCli;
+            nomClient = nomCli;
+            prenomClient = prenomCli;
+        }
+
+        public string getCivClient() { return civClient; }
+        public string getNomClient() { return nomClient; }
+        public string getPrenomClient() { return prenomClient; }
+    }
+}
