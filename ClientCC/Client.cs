aab5ce8ecde58da8c6ef9596409a7efa90c3d1f9
Daniels SOARES <danielo-24@hotmail.com>
2025-02-28 23:44:35 +0100

    Ajout de fonctionnalités à la classe Client
    Modifications apportées dans `Client.cs` :
    - Ajout de l'attribut `NumClient` et de la liste `lesClients`.
    - Création de deux nouveaux constructeurs pour initialiser les clients.
    - Ajout des méthodes `getInfoClients` et `getIDClient` pour récupérer des informations sur le client.
    
    Changements dans `desktop.ini` :
    - Suppression de lignes dans la section `[.ShellClassInfo]` sans ajout de nouvelles lignes.

--- ClientCC/Client.cs
+++ ClientCC/Client.cs
@@ -11,14 +11,27 @@ namespace CreditCeleste
         private string civClient;
         private string nomClient;
         private string prenomClient;
+        private int NumClient;
+
+        private List<Client> lesClients = new List<Client>();
 
         public Client()
         {
+            NumClient = 1;
             civClient = "Mr";
             nomClient = "TirLudAleDan";
             prenomClient = "TLAD";
 
         }
+
+        public Client(int NumC)
+        {
+            NumClient = NumC;
+            civClient = "Mr";
+            nomClient = "TirLudAleDan";
+            prenomClient = "TLAD";
+        }
+
 
         public Client(string CivCli)
         {
@@ -41,6 +54,15 @@ namespace CreditCeleste
             prenomClient = prenomCli;
         }
 
+        public Client(int NumC, string CivCli, string nomCli, string prenomCli)
+        {
+            NumClient = NumC;
+            civClient = CivCli;
+            nomClient = nomCli;
+            prenomClient = prenomCli;
+        }
+
+
         public void setClient(string CivCli, string nomCli, string prenomCli)
         {
             civClient = CivCli;
@@ -51,5 +73,16 @@ namespace CreditCeleste
         public string getCivClient() { return civClient; }
         public string getNomClient() { return nomClient; }
         public string getPrenomClient() { return prenomClient; }
+
+        public string getInfoClients()
+        {
+            return civClient + " " + nomClient + " " + prenomClient;
+        }
+
+        public int getIDClient()
+        {
+            return NumClient;
+        }
+
     }
 }
